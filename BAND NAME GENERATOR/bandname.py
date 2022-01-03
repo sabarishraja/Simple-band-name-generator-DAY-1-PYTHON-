e5f2176b3c426ce4abd93cd800fc6c4cd68e808d
R.SABARISH RAJA <80580833+sabarishraja@users.noreply.github.com>
2022-01-03 23:03:06 +0530

    Add files via upload

---
+++ BAND NAME GENERATOR/bandname.py
@@ -0,0 +1,4 @@
+print("Welcome to Band Name Generator by Sabarish")
+a = input("What's the name of the city you grew up in?\n")
+b = input("What's your pet's name?\n")
+print("Your band can be named as " + a + " " + b)
